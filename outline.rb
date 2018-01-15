a5e7322da99d64c0d56ce919a3311680adce0e43
Thomas Conkwright <tmconkwright@gmail.com>
2018-01-15 07:21:08 -0500

    Rails set-up

---
+++ outline.rb
@@ -0,0 +1,133 @@
+Brewright outline/to-do list (Brewtoad + OBeer done wright)
+    Brewing - priority
+        recipe - priority
+            ingredients - priority
+                malt/fermentables - priority
+                    ppg?
+                    color - priority
+                    modification
+                    diastaic power - priority
+                    usage rate
+                    extract - priority
+                hops - priority
+                    alpha acid - priority
+                    purpose
+                additives - priority
+                    color
+                    salts (in mash for pH adjustment or boil for mouth feel)
+                water
+                yeast - priority
+                    attenutation - priority
+                    temp range
+                    floculation
+            steps - priority
+                mash - priority
+                    mash water/temp - priority
+                    mash steps - priority
+                    sparge water/temp - priority
+                    mash pH
+                    decoction options for the mentally ill
+                lauter - priority
+                kettle - priority
+                    boil time - priority
+                    boil additions - priority
+                    kettle sour
+                whirlpool
+                knockout - priority
+                    temperature - priority
+                    oxygen
+                    dosing/process
+                yeast count - priority
+            brew profile/predicted outcome - priority
+                og - priority
+                fg - priority
+                abv - priority
+                ibu - priority
+                color - priority
+                character
+                    sweetness
+                    aroma
+                    bitterness
+                    body
+                    mouthfeel
+        brewsheet creation  - priority
+            day of changes
+            alpha acid degredation
+        style profiles
+            possibly auto populate beer styles
+            show/compare beer style parameters to brew
+            description
+            indicate that brew uses some/all traditional ingrediets
+            suggestions to conform to syle
+            
+    Cellaring - priority
+        fermantaion profile
+            temperature/temp changes
+            dosing additions
+                dry hop
+                spices
+                additions
+            coldcrash
+        conditioning
+            lagering
+            aging
+            souring
+            barrels
+        transfer
+            dosing
+            clarification
+            BT
+        carbonation - priority
+
+
+    Packaging
+    Lab
+        fermentation monitoring
+            temperature
+            gravity
+            ph
+        yeast wrangling
+            strains
+            cell count
+            yeast prop/brink
+    Inventory
+        ingredients
+        brewery
+            Brewhouse
+            FVs
+            CTs
+            BTs
+            filter/centrifuge
+            barrels
+            CIP
+            yeast brink
+            water treatment
+            waste treatment
+            
+        packaging/cooperage
+            bottles
+            cans
+            kegs
+            packaging
+                case trays
+                case boxes
+        
+        finished product
+            cans
+            bottles
+            kegs
+            taproom
+        supplies
+            safety equipment
+            office supplies
+        machinery
+    Scheduling
+    Finances
+        Incoming
+            taproom
+            distribution
+        Outgoing
+            ingrediets
+            taxes
+            fees
+        
