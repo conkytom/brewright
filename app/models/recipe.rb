464fbe26aa5e52fb9353ee39d5a79e55955e2bf7
Thomas Conkwright <tmconkwright@gmail.com>
2018-02-03 10:09:45 -0500

    Added sample ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,7 @@ class Recipe < ApplicationRecord
     has_one :fermentation
     has_one :equipment
 
+    default_scope { order('name ASC') }
+
     validates :name, length: {minimum: 1}, presence: true
 end
